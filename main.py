3f557c368a644a580549a0282f64efb79d56b4ae
dayojohn19 <dayo_john16@yahoo.com>
2025-01-17 15:17:06 +0800

    v8

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ files_to_update=["main.py","configs/listnames.txt","configs/esp12settings.json",
 giturl= "https://github.com/dayojohn19/esp12f/"
 
 
-print("get try versioning 111")
+print("get try versioning 111 for v8")
 
 
 
